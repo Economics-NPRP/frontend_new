1bdb31f8ea5837bb1f711781fd970df3f04973ac
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2025-01-09 09:39:21 +0300

    fix: auth running too often and failing

--- src/lib/auth/sessionUtils.ts
+++ src/lib/auth/sessionUtils.ts
@@ -1,29 +1,13 @@
 import { jwtDecode } from 'jwt-decode';
-import { cookies, headers } from 'next/headers';
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import 'server-only';
-
-import { IAccessToken, IRefreshToken } from '@/types';
-import { ipAddress } from '@vercel/functions';
 
 const DEV_LOGIN = new FormData();
 DEV_LOGIN.append('username', process.env.SUPERUSER_EMAIL!);
 DEV_LOGIN.append('password', process.env.SUPERUSER_PASSWORD!);
 
-const QUERY_SETTINGS: RequestInit = {
-	method: 'POST',
-	body: DEV_LOGIN,
-};
-
-export const createSession = async (req: NextRequest, res: NextResponse) => {
-	//	Login as superuser for development purposes
-	const queryUrl = new URL('/dev/auth/login', process.env.NEXT_PUBLIC_BACKEND_URL);
-	const response = await fetch(queryUrl, QUERY_SETTINGS);
-
-	if (!response.ok) return response;
-	if (!response.headers || response.headers.getSetCookie().length === 0) return response;
-
-	//	Take cookies from backend and set them in the frontend
+const saveCookies = (response: Response, res: NextResponse) => {
 	let accessToken = '',
 		refreshToken = '',
 		sessionExp = 0;
@@ -47,26 +31,32 @@ export const createSession = async (req: NextRequest, res: NextResponse) => {
 		}
 	});
 
-	//	Add session cookie to the response
-	res.cookies.set(
-		'ets_session',
-		`ets_access_token=${accessToken}; ets_refresh_token=${refreshToken}`,
-		{
-			httpOnly: true,
-			secure: true,
-			expires: sessionExp,
-			sameSite: 'lax',
-			path: '/',
-		},
-	);
+	return {
+		sessionCookie: `ets_access_token=${accessToken}; ets_refresh_token=${refreshToken}`,
+		sessionExp,
+	};
 };
 
-//	TODO: move to separate util file
-const getIpAddress = async (req: NextRequest) => {
-	if (process.env.NODE_ENV === 'development') {
-		const header = await headers();
-		return header.get('x-forwarded-for') || header.get('x-real-ip') || '127.0.0.1';
-	} else return ipAddress(req); //	Use the ipAddress function from @vercel/functions
+export const createSession = async (req: NextRequest, res: NextResponse) => {
+	//	Login as superuser for development purposes
+	const queryUrl = new URL('/dev/auth/login', process.env.NEXT_PUBLIC_BACKEND_URL);
+	const response = await fetch(queryUrl, {
+		method: 'POST',
+		body: DEV_LOGIN,
+	});
+
+	if (!response.ok) return response;
+	if (!response.headers || response.headers.getSetCookie().length === 0) return response;
+
+	//	Take cookies from backend and set them in the frontend
+	const { sessionCookie, sessionExp } = saveCookies(response, res);
+	res.cookies.set('ets_session', sessionCookie, {
+		httpOnly: true,
+		secure: true,
+		expires: sessionExp,
+		sameSite: 'lax',
+		path: '/',
+	});
 };
 
 export const verifySession = async (req: NextRequest, res: NextResponse) => {
@@ -76,21 +66,25 @@ export const verifySession = async (req: NextRequest, res: NextResponse) => {
 	//	Initial check for tokens
 	if (!accessToken || !refreshToken) return null;
 
-	//	Check if tokens are valid and belong to the same user
-	const ip = await getIpAddress(req);
-	const { jti: atJti, sub: atSub, exp: atExp } = jwtDecode<IAccessToken>(accessToken.value);
-	const { jti: rtJti, sub: rtSub, ip: rtIp } = jwtDecode<IRefreshToken>(refreshToken.value);
-	const sameJti = atJti === rtJti;
-	const sameSub = atSub === rtSub;
-	const sameIp = process.env.NODE_ENV === 'development' ? true : ip === rtIp;
-	if (!sameJti || !sameSub || !sameIp) return null;
+	//	Check with the server and refresh the tokens
+	const initialSessionCookie = `ets_access_token=${accessToken.value}; ets_refresh_token=${refreshToken.value}`;
+	const queryUrl = new URL('/v1/auth/refresh', process.env.NEXT_PUBLIC_BACKEND_URL);
+	const response = await fetch(queryUrl, {
+		method: 'POST',
+		headers: {
+			Cookie: initialSessionCookie,
+		},
+	});
+
+	//	If the response is not ok, return null
+	if (!response.ok) return null;
 
 	//	In all other cases, return the session cookie
-	const sessionCookie = `ets_access_token=${accessToken.value}; ets_refresh_token=${refreshToken.value}`;
+	const { sessionCookie, sessionExp } = saveCookies(response, res);
 	res.cookies.set('ets_session', sessionCookie, {
 		httpOnly: true,
 		secure: true,
-		expires: Date.now() + (atExp || 0),
+		expires: sessionExp,
 		sameSite: 'lax',
 		path: '/',
 	});
