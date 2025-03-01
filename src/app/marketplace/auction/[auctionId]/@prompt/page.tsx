a5a000d74154195bfb999897c718aeb966a1bb77
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2025-03-01 15:15:13 +0300

    feat: add bid form validation

--- src/app/marketplace/auction/[auctionId]/@prompt/page.tsx
+++ src/app/marketplace/auction/[auctionId]/@prompt/page.tsx
@@ -2,15 +2,17 @@
 
 import { DateTime } from 'luxon';
 import { useFormatter } from 'next-intl';
-import { useContext, useMemo, useState } from 'react';
+import { useCallback, useContext, useMemo, useState } from 'react';
 
 import { CurrencyBadge } from '@/components/Badge';
 import { LargeCountdown } from '@/components/Countdown';
 import {
 	ActionIcon,
+	Alert,
 	Button,
 	Checkbox,
 	Group,
+	List,
 	NumberInput,
 	Progress,
 	Stack,
@@ -24,7 +26,7 @@ import {
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useListState } from '@mantine/hooks';
-import { IconChartLine } from '@tabler/icons-react';
+import { IconChartLine, IconExclamationCircle } from '@tabler/icons-react';
 
 import { AuctionDetailsContext } from '../constants';
 
@@ -40,16 +42,31 @@ export default function Prompt() {
 	const [subtotal, setSubtotal] = useState(0);
 	const [bids, bidsHandlers] = useListState<FormValues>();
 
-	const form = useForm<FormValues>({
-		mode: 'uncontrolled',
-		onValuesChange: ({ bid, permit }) => setSubtotal(Number(bid || 0) * Number(permit || 0)),
-	});
-
 	const totalPermits = useMemo(() => bids.reduce((acc, { permit }) => acc + permit, 0), [bids]);
 	const grandTotal = useMemo(
 		() => bids.reduce((acc, { bid, permit }) => acc + bid * permit, 0),
 		[bids],
 	);
+
+	const form = useForm<FormValues>({
+		mode: 'uncontrolled',
+		onValuesChange: ({ bid, permit }) => setSubtotal(Number(bid || 0) * Number(permit || 0)),
+		validate: {
+			permit: (value) => {
+				if (!value) return 'Permit is required';
+				if (value < 1) return 'You must bid at least 1 permit';
+				if (value > auctionData.permits) return 'You cannot bid more than permits offered';
+				if (value > auctionData.permits - totalPermits)
+					return 'You cannot bid more than available permits. Please check the bid table';
+				return false;
+			},
+			bid: (value) => {
+				if (!value) return 'Bid is required';
+				if (value < 1) return 'Bid must be greater than 0';
+				return false;
+			},
+		},
+	});
 
 	const bidsData = useMemo(
 		() =>
@@ -71,6 +88,22 @@ export default function Prompt() {
 				</TableTr>
 			)),
 		[bids],
+	);
+
+	const errorMessages = useMemo(
+		() =>
+			Object.values(form.errors).map((error, index) => (
+				<List.Item key={index}>{error}</List.Item>
+			)),
+		[form.errors],
+	);
+
+	const onSubmitHandler = useCallback(
+		(values: FormValues) => {
+			bidsHandlers.append(values);
+			form.reset();
+		},
+		[bidsHandlers],
 	);
 
 	return (
@@ -104,8 +137,18 @@ export default function Prompt() {
 					</ActionIcon>
 				</Stack>
 				<Stack>
-					<Group>
-						<form onSubmit={form.onSubmit((value) => bidsHandlers.append(value))}>
+					{errorMessages.length > 0 && (
+						<Alert
+							variant="light"
+							color="red"
+							title="There was a problem adding your bid"
+							icon={<IconExclamationCircle />}
+						>
+							<List>{errorMessages}</List>
+						</Alert>
+					)}
+					<form onSubmit={form.onSubmit(onSubmitHandler)}>
+						<Group>
 							<NumberInput
 								placeholder="000,000"
 								min={1}
@@ -124,8 +167,8 @@ export default function Prompt() {
 							<Text>Each</Text>
 							<Text>Total QAR {format.number(subtotal, 'money')}</Text>
 							<Button type="submit">Add to List</Button>
-						</form>
-					</Group>
+						</Group>
+					</form>
 					<Group>
 						<Text>{totalPermits} permits bid</Text>
 						<Progress w={480} value={(totalPermits / auctionData.permits) * 100} />
