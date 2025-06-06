44c23d1f6c6092f15fa5352bc3d2acabc2e999a3
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2025-06-06 10:28:26 +0300

    feat: prevent user from leaving during bidding

---
+++ src/hooks/useUnsavedChanges.ts
@@ -0,0 +1,17 @@
+import { useCallback } from 'react';
+
+import { useWindowEvent } from '@mantine/hooks';
+
+export const useUnsavedChanges = (isDirty: boolean) => {
+	const handler = useCallback(
+		(e: BeforeUnloadEvent) => {
+			console.log(isDirty);
+			if (!isDirty) return;
+			e.preventDefault();
+			e.returnValue = '';
+		},
+		[isDirty],
+	);
+
+	useWindowEvent('beforeunload', handler);
+};
